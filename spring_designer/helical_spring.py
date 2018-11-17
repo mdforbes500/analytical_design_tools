3838b679c979af92f40a23adde1eb91e9f6f04b8
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-11-17 18:12:48 -0500

    Now with databases!

--- spring_designer/helical_spring.py
+++ spring_designer/helical_spring.py
@@ -2,4 +2,13 @@
 
 import sqlite3
 
+print('Connecting to database...')
 conn = sqlite3.connect('../tables/test.dB')
+print('Connection successful!')
+
+cursor = conn.execute('''
+select material, exponent, A_metric from table10_4
+''')
+material = cursor.fetchone()
+
+print(material[0])
