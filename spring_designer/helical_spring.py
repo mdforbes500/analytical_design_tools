e631b7f2e3ff95a88d9bfe157cc35462e32e78d6
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-11-19 11:14:12 -0500

    Can retreive relevant quantities from table10_4 and get ultimate strength

--- spring_designer/helical_spring.py
+++ spring_designer/helical_spring.py
@@ -2,13 +2,38 @@
 
 import sqlite3
 
-print('Connecting to database...')
-conn = sqlite3.connect('../tables/test.dB')
-print('Connection successful!')
+class HelicalCompressionSpring:
+    """A class for the design of helical compression springs. Based upon
+    Shigley's Mechanical Engineering Design."""
 
-cursor = conn.execute('''
-select material, exponent, A_metric from table10_4
-''')
-material = cursor.fetchone()
+    def __init__(self, d, D, C, OD, Na, Ls, L0, L0cr, ns, fom):
+        """Constructor method for the HelicalCompressionSpring class.
+        Takes a priori decisions and sets them as the initial guess."""
+        self.material = "Hard-drawn wire"
+        self.d = d
+        self.D = D
+        self.C = C
+        self.OD = OD
+        self.Na = Na
+        self.Ls = Ls
+        self.L0 = L0
+        self.L0cr = L0cr
+        self.ns = ns
+        self.fom = fom
 
-print(material[0])
+    def getUltimateStrength(self):
+        """Takes HelicalCompressionSpring object and uses diameter and
+        material to find the ultimate strength of the spring."""
+
+        print('Connecting to database...')
+        conn = sqlite3.connect('../tables/test.dB')
+        print('Connection successful!')
+
+        cursor = conn.execute('''
+        select material, exponent, A_metric from table10_4
+        where material = ?
+        ''', (self.material))
+
+        entry = cursor.fetchone()
+
+        return (A/d**m)
