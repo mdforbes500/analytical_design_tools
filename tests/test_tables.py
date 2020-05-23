176e95235dde71b6f59d5efdb2dffb4efcdd5127
Malcolm Forbes <malcolm.forbes500@gmail.com>
2020-05-23 17:21:47 -0400

    Turned it into something more useful and less general

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -6,18 +6,10 @@ import unittest
 
 class TestUtilites(unittest.TestCase):
 
-    def test_importTable(self):
-        testData = pd.read_csv(
-            "artifacts/test_data/A-28.csv",
-            header=0
-        )
-        column_title = testData.columns[0]
-        testData.set_index(column_title, inplace=True)
+    def test_findDiameter(self):
         material = 'Music Wire'
         guage = '16'
-
-        value = util.tables.lookup_property(testData, guage, material)
-
+        value = util.tables.find_guage_diameter(guage, material)
         self.assertEqual(value, 0.037)
 
 if __name__ == "__main__":
--- utilities/tables.py
+++ utilities/tables.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import os
 
-def lookup_property(df: pd.DataFrame, props: str, material: str) -> None:
+def find_guage_diameter(props: str, material: str) -> None:
     """
     Accepts the table as a Dataframe object, property (props) and material.
     Returns the value of the material property and units assoicated with it.
     """
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + "/data/A-28.csv"
+    df = pd.read_csv(path, header=0)
+    column_title = df.columns[0]
+    df.set_index(column_title, inplace=True)
     return float(df.at[props, material]) 
